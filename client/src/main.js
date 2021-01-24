690c81c89093a96450718b96b6761d3a8781f991
tmerry <tymerry@gmail.com>
2021-01-24 15:46:19 -0700

    starting over

--- client/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from "./Router"
-
-
-createApp(App).use(router).mount('#app')
--- server.js
+++
@@ -1,9 +0,0 @@
-const server = require('server')
-const { get } = require('server/router')
-
-const port = process.env.PORT || 3000
-
-server({ port }, [
-    get('/', ctx => '<h1>Hey you</h1>')
-])
-    .then(()=> console.log(`cherver running at http://localhost:${port}`))
